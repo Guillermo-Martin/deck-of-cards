68ffeef29927a26351f5678415c1094f495e9fff
gcmartin <gscalica@gmail.com>
2020-09-09 13:41:25 -0700

    Add state and add axios request and setState to componentDidMount

--- src/components/Dealer.js
+++ src/components/Dealer.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Dealer extends Component {
+  state  = {
+    remaining: "",
+    deckId: ""
+  }
 
-  componentDidMount() {
-    console.log("Inside componentDidMount");
+  async componentDidMount() {
+    console.log("inside componentDidMount")
+    // when page loads, send a request to https://deckofcardsapi.com/api/deck/new/shuffle
+    let response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle");
+    
+    // change remaining and deck id state to data i get back from the response
+    this.setState({
+      remaining: response.data.remaining,
+      deckId: response.data.deck_id
+    });
+    
   }
 
   render() {
@@ -18,7 +31,7 @@ class Dealer extends Component {
 
 export default Dealer;
 
-// when page loads, send a request to https://deckofcardsapi.com/api/deck/new/shuffle
+
 // request will go to componentDidMount();
 // store deck info in localStorage?
 // button will make an api request to https://deckofcardsapi.com/api/deck/$%7Bdeck_id%7D/draw/
