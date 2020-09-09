ffd80977f2a89750b1e6924bd746fae015c0c5c0
gcmartin <gscalica@gmail.com>
2020-09-08 17:23:36 -0700

    Create Dealer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from './components/Card';
+import Dealer from './components/Dealer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Deck of Cards</h1>
-      <Card />
+      <Dealer />
     </div>
   );
 }
---
+++ src/components/Dealer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dealer extends Component {
+  render() {
+    return (
+      <div>
+        <button>Deal a Card</button>
+      </div>
+    );
+  }
+}
+
+export default Dealer;
