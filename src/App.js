7b550d0d99794d774a0c26a93fee671e8fa9bdc2
gcmartin <gscalica@gmail.com>
2020-09-08 17:15:51 -0700

    Create components folder and Card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Deck of Cards</h1>
+      <Card />
     </div>
   );
 }
