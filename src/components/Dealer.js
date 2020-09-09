284daa22ca0f16be71ae1698bed05a1d946fbe2f
gcmartin <gscalica@gmail.com>
2020-09-09 15:10:51 -0700

    Create dealCard function

--- src/components/Dealer.js
+++ src/components/Dealer.js
@@ -4,26 +4,45 @@ import axios from 'axios';
 class Dealer extends Component {
   state  = {
     remaining: "",
-    deckId: ""
+    deckId: "",
+    allCards: []
   }
+
+  
 
   async componentDidMount() {
     console.log("inside componentDidMount")
     // when page loads, send a request to https://deckofcardsapi.com/api/deck/new/shuffle
     let response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle");
-    
+    // console.log(response)
     // change remaining and deck id state to data i get back from the response
     this.setState({
       remaining: response.data.remaining,
       deckId: response.data.deck_id
     });
-    
   }
+
+  // button function
+  dealCard = () => {
+    // alert("connected!");
+    // make an api request to https://deckofcardsapi.com/api/deck/$%7Bdeck_id%7D/draw/
+    axios
+      .get(`https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/`)
+      .then(response =>  {
+        // console.log(response.data.cards[0].images.png, "line 32");
+        // take the card image, push it into an array (allCards state)
+        let newCard = response.data.cards[0].images.png;
+        this.setState(state => (
+          {allCards: [...state.allCards, newCard]})
+        )});
+  }
+
 
   render() {
     return (
       <div>
-        <button>Deal a Card</button>
+        <button onClick={this.dealCard}>Deal a Card</button>
+        <h2>{this.state.allCards}</h2>
       </div>
     );
   }
@@ -34,7 +53,10 @@ export default Dealer;
 
 // request will go to componentDidMount();
 // store deck info in localStorage?
-// button will make an api request to https://deckofcardsapi.com/api/deck/$%7Bdeck_id%7D/draw/
+// button will 
 // render card after making the request
 
-// next commit:  add componentDidMount
+
+// will change remaining card number
+// render an card component 
+// map over that array, and create a card component for every image in the array
