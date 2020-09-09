4627bb5f920c2e397b8755fc2d002e8840f4d1b1
gcmartin <gscalica@gmail.com>
2020-09-09 13:21:09 -0700

    Add componentDidMount to Dealer and add Axios as a dependency

--- src/components/Dealer.js
+++ src/components/Dealer.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Dealer extends Component {
+
+  componentDidMount() {
+    console.log("Inside componentDidMount");
+  }
+
   render() {
     return (
       <div>
@@ -11,3 +17,11 @@ class Dealer extends Component {
 }
 
 export default Dealer;
+
+// when page loads, send a request to https://deckofcardsapi.com/api/deck/new/shuffle
+// request will go to componentDidMount();
+// store deck info in localStorage?
+// button will make an api request to https://deckofcardsapi.com/api/deck/$%7Bdeck_id%7D/draw/
+// render card after making the request
+
+// next commit:  add componentDidMount
